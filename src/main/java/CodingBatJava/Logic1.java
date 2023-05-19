467fc5409463cca3d6beacf2f1a4c106c0adc149
Mikal McLain <126420515+Deminei@users.noreply.github.com>
2023-05-19 15:43:10 -0400

    Update Logic1.java
    fixed the wrong typo last time. fixed the fix, and the right typo this time. I need a nap.

--- src/main/java/CodingBatJava/Logic1.java
+++ src/main/java/CodingBatJava/Logic1.java
@@ -2,8 +2,8 @@ package CodingBatJava;
 
 public class Logic1 {
     public boolean cigarParty(int cigars, boolean isWeekend) {
-        if(cigars >= 40 && cigars <= 60){  //Well if you don't now how much I like
-            return true;                   //comparison operators, you do know.
+        if(cigars >= 40 && cigars <= 60){  //Well if you don't know how much I like
+            return true;                   //comparison operators, you do now.
         }else if(cigars > 40 && isWeekend){//I realize there is better or shorter ways
             return true;                   //of logicing this logic, but it worked!
         }else if(cigars < 40){              //that's what counts right? ...right!?
